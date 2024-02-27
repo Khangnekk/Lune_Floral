728ed9f605a7a39a9c8db206367ae95f8ddd13bb
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2024-02-27 20:40:11 +0700

    init project

---
+++ Lune_Floral/Lune_Floral/Program.cs
@@ -0,0 +1,11 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+//builder.Services.AddDbContext<CinemaContext>(
+//	opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("MyConnectionStrings")));
+
+var app = builder.Build();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapRazorPages();
+
+app.Run();
